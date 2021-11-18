37655ab36f0252e1b02895e4ff732c043a0b0df0
arakotomahefa <alison.rakotomahefa@arolla.fr>
2021-11-18 19:43:39 +0100

    test_should_return_sum_fraction

--- fraction_test.py
+++ fraction_test.py
@@ -1,9 +1,21 @@
-from kata_fraction import Fraction
+from kata_fraction import Fraction,sum_fraction
 
-def test_should_return_sum_2_fractions():
+def test_should_return_a_fraction():
     actual = Fraction(3,2)
     expected = Fraction(3,2)
     assert actual.numerateur == expected.numerateur
     assert actual.denominateur == expected.denominateur
+
+
+def test_should_return_sum_fraction():
+    fract_1 = Fraction(3,2)
+    fract_2 = Fraction(3,2)
+    actual = sum_fraction(fract_1,fract_2)
+    expected = Fraction(12,4)
+    assert isinstance(actual,Fraction)
+    assert actual.numerateur == expected.numerateur
+    assert actual.denominateur == expected.denominateur
+
+
 
 
--- kata_fraction.py
+++ kata_fraction.py
@@ -1,3 +1,8 @@
+def sum_fraction(fract_A, fract_B):
+  return Fraction((fract_A.numerateur * fract_B.denominateur + fract_B.numerateur * fract_A.denominateur),
+                  fract_A.denominateur * fract_B.denominateur)
+
+
 class Fraction:
   def __init__(self, numerateur, denominateur):
     self.numerateur = numerateur
