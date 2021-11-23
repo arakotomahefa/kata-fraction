1e2abf1700f3936138b396e708ccb540b9bcf9bb
arakotomahefa <alison.rakotomahefa@arolla.fr>
2021-11-23 23:36:38 +0100

    test_should_return_gcd_fraction + test_should_return_reduced_fraction

--- fraction_test.py
+++ fraction_test.py
@@ -1,4 +1,4 @@
-from kata_fraction import Fraction,sum_fraction
+from kata_fraction import Fraction,sum_fraction, gcd, reduced_fraction
 
 def test_should_return_a_fraction():
     actual = Fraction(3,2)
@@ -9,13 +9,25 @@ def test_should_return_a_fraction():
 
 def test_should_return_sum_fraction():
     fract_1 = Fraction(3,2)
-    fract_2 = Fraction(3,2)
+    fract_2 = Fraction(3,5)
     actual = sum_fraction(fract_1,fract_2)
-    expected = Fraction(12,4)
+    expected = Fraction(21,10)
     assert isinstance(actual,Fraction)
     assert actual.numerateur == expected.numerateur
     assert actual.denominateur == expected.denominateur
 
+def test_should_return_gcd_fraction():
+    a = 2
+    b = 4
+    actual = gcd(a,b)
+    expected = 2
+    assert actual == expected
 
-
-
+def test_should_return_reduced_fraction():
+    fract_1 = Fraction(3, 2)
+    fract_2 = Fraction(3, 4)
+    actual = reduced_fraction(fract_1,fract_2)
+    expected = Fraction(9, 4)
+    assert isinstance(actual,Fraction)
+    assert actual.numerateur == expected.numerateur
+    assert actual.denominateur == expected.denominateur
--- kata_fraction.py
+++ kata_fraction.py
@@ -2,8 +2,16 @@ def sum_fraction(fract_A, fract_B):
   return Fraction((fract_A.numerateur * fract_B.denominateur + fract_B.numerateur * fract_A.denominateur),
                   fract_A.denominateur * fract_B.denominateur)
 
+def gcd(a,b):
+    return a if b==0 else gcd(b,a%b)
+
+def reduced_fraction(a,b):
+    val_gcd = gcd(sum_fraction(a,b).numerateur,sum_fraction(a,b).denominateur)
+    return Fraction(sum_fraction(a,b).numerateur/val_gcd,sum_fraction(a,b).denominateur/val_gcd)
 
 class Fraction:
   def __init__(self, numerateur, denominateur):
     self.numerateur = numerateur
     self.denominateur = denominateur
+
+
